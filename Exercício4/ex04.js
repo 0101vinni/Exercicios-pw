fd11982ec9669fb97ffc2694d5cf4cc21d47084f
devinni <119471051+devinni@users.noreply.github.com>
2022-11-30 00:35:14 -0300

    Update ex04.js

--- Exercício4/ex04.js
+++ Exercício4/ex04.js
@@ -1,4 +1,40 @@
+let people = [{ name: "VAZIO", birth_year: "VAZIO", height: "VAZIO", height: "VAZIO", mass: "VAZIO", hair_color: "VAZIO", skin_color: "VAZIO", eye_color: "VAZIO"}];
+let currentPage = "https://swapi.dev/api/people";
 
+const btFetch = document.getElementById("btFetch");
+const listaFetch = document.getElementById("listaFetch");
+const tabela= document.getElementById("tabela");
+
+function exibirListaStarWars() {
+  tabela.innerHTML = "";
+  let tdNode;
+  let textNode;
+
+  for (let i = 0; i < people.length; ++i) {
+      const trNode = document.createElement("tr");
+
+      textNode = document.createTextNode(
+          `${people[i].name}`
+      );
+      tdNode = document.createElement("td");
+      tdNode.appendChild (textNode);
+      trNode.appendChild(tdNode);
+
+
+      textNode = document.createTextNode(
+          `${people[i].birth_year}`
+      );
+      tdNode = document.createElement("td");
+      tdNode.appendChild (textNode);
+      trNode.appendChild(tdNode);
+
+
+      textNode = document.createTextNode(
+          `${people[i].height}`
+      );
+      tdNode = document.createElement("td");
+      tdNode.appendChild (textNode);
+      trNode.appendChild(tdNode);
 
 
       textNode = document.createTextNode(
